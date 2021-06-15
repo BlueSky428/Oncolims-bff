12d557327ceb46d7f21550827b89d9e0e117dcab
BlueSky428 <bluesky.engineer428@gmail.com>
2021-06-14 22:46:29 -0400

    fun: added random avatar generator

--- oncolims-bff/ClientApp/src/components/Layouts/Layout.tsx
+++ oncolims-bff/ClientApp/src/components/Layouts/Layout.tsx
@@ -3,6 +3,7 @@ import { Sidebar, IProfileFooter, ProfileFooter, SidebarContent, SidebarProps } 
 import useClaim from '../../apis/auth/claims'
 import { UseQueryResult } from 'react-query'
 import restricted from '../../Images/safe.png'
+import Avatar, { genConfig, AvatarConfig } from 'react-nice-avatar'
 
 function Layout({children}: SidebarProps) {
 
@@ -12,6 +13,12 @@ function Layout({children}: SidebarProps) {
   let logoutUrl = data?.find(claim => claim.type === 'bff:logout_url') 
   let nameDict = data?.find(claim => claim.type === 'name') ||  data?.find(claim => claim.type === 'sub');
   let name = nameDict?.value;
+
+  const [avatarConfig, setAvatarConfig] = React.useState(null)
+  React.useLayoutEffect(() => {
+    let config = GetRandomAvatarConfigForFun(name);
+    setAvatarConfig(config);
+  }, [name])
 
   return (    
     <div className="h-screen flex overflow-hidden bg-gray-50">
@@ -29,11 +36,12 @@ function Layout({children}: SidebarProps) {
               <div className="flex-shrink-0 group block md:w-full ">
                 <div className="flex items-center">
                   <div>  
-                    <img
+                    {/* <img
                       className="inline-block h-10 w-10 md:h-9 md:w-9 rounded-full"
                       src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                       alt={`${name} Avatar`}
-                    />
+                    /> */}
+                    <Avatar className="inline-block h-10 w-10 md:h-9 md:w-9 rounded-full" {...avatarConfig} />
                   </div>
                   <div className="ml-3">
                     <p className="block text-base font-medium text-white md:text-sm">{name}</p>
@@ -66,6 +74,24 @@ function Layout({children}: SidebarProps) {
       </SidebarContent>
     </div>
   )
+}
+
+function GetRandomAvatarConfigForFun(name: string | undefined, ) {
+  console.log(name)
+  const random = (items: string[]) => items[Math.floor(Math.random()*items.length)];
+  const calculatedSex = name === "Bob Smith" ? 'man' : 'woman'
+  const earSize = ['small', 'big']
+  const hairStyle = ['normal', 'thick', 'mohawk', 'womanLong', 'womanShort']
+  const eyeStyle = ['circle', 'oval', 'smile']
+  const glassesStyle = ['round', 'square', 'none']
+  const noseStyle = ['short', 'long', 'round']
+  const mouthStyle = ['laugh', 'smile', 'peace']
+  const shirtStyle = ['hoody', 'short', 'polo']
+  
+  let randomAvatarConfig: AvatarConfig = {
+    sex: calculatedSex,
+  }
+  return genConfig(randomAvatarConfig)
 }
 
 export default Layout
